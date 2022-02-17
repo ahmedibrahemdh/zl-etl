4f757b73dbbd6990024ed203deb90a42f3535381
dmdesimone <ddesimone@pih.org>
2022-02-17 16:10:59 +0100

    uhm-6205 Additional columns on hiv_visit  table
    uhm-6205 Additional columns on hiv_visit  table

--- jobs/sql/schemas/hiv_visit.sql
+++ jobs/sql/schemas/hiv_visit.sql
@@ -3,8 +3,12 @@ CREATE TABLE hiv_visit
     encounter_id    INT,
     patient_id      INT,
     emr_id          VARCHAR(25),
+	hivemr_v1       VARCHAR(25),
+	encounter_type 	varchar(255),	
     pregnant        BIT,
     visit_date      DATE,
     next_visit_date DATE,
-    visit_location  varchar(255)
+    visit_location  varchar(255),
+  	index_asc		int,
+	index_desc		int
 );
