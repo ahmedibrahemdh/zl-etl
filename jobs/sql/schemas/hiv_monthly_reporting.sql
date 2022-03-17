8a6670a05b5867855404626be5a2e56f452d2631
dmdesimone <ddesimone@pih.org>
2022-03-17 17:05:02 +0100

    Update hiv_monthly_reporting.sql

--- jobs/sql/schemas/hiv_monthly_reporting.sql
+++ jobs/sql/schemas/hiv_monthly_reporting.sql
@@ -1,20 +1,16 @@
 create table hiv_monthly_reporting
 (
-	patient_id 						int,
-  	zl_emr_id						varchar(255),
-  	date_enrolled 					datetime,
-  	date_completed 					datetime,
-    reporting_date					date,
-	latest_HIV_note_encounter_id	int,
-    latest_HIV_visit_date			datetime,
-    latest_expected_HIV_visit_date	datetime,
-	HIV_visit_days_late				int,
-	latest_dispensing_encounter_id	int,
-    latest_dispensing_date			datetime,
-    latest_expected_dispensing_date	datetime,
-	dispensing_days_late			int
-);
-  	
-
-
-	
+patient_id 			int,
+zl_emr_id			varchar(255),
+date_enrolled 			datetime,
+date_completed 			datetime,
+reporting_date			date,
+latest_hiv_note_encounter_id	int,
+latest_hiv_visit_date		datetime,
+latest_expected_hiv_visit_date	datetime,
+hiv_visit_days_late		int,
+latest_dispensing_encounter_id	int,
+latest_dispensing_date		datetime,
+latest_expected_dispensing_date	datetime,
+dispensing_days_late		int
+); 
